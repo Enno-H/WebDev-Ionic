3ec2be8b56d27e907e3dd55df8ef46c1fd197c42
Xiaolong Huang <xiaolong_huang@foxmail.com>
2018-02-11 00:12:21 +1300

    Ionic Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomePage } from '../pages/home/home';
 import { AboutPage } from '../pages/about/about';
 import { MenuPage } from '../pages/menu/menu';
 import { ContactPage } from '../pages/contact/contact';
+import { DishdetailPage } from '../pages/dishdetail/dishdetail';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -26,6 +27,7 @@ import { baseURL } from '../shared/baseurl';
     AboutPage,
     MenuPage,
     ContactPage,
+    DishdetailPage,
   ],
   imports: [
     BrowserModule,
@@ -39,6 +41,7 @@ import { baseURL } from '../shared/baseurl';
     AboutPage,
     MenuPage,
     ContactPage,
+    DishdetailPage,
   ],
   providers: [
     StatusBar,
