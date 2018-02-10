bdd7fec6ecfa25c545226d836d816fe3c43ef074
Xiaolong Huang <xiaolong_huang@foxmail.com>
2018-02-10 16:10:10 +1300

    Ionic Angular Part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,14 @@ import { ContactPage } from '../pages/contact/contact';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { DishProvider } from '../providers/dish/dish';
+import { LeaderProvider } from '../providers/leader/leader';
+import { PromotionProvider } from '../providers/promotion/promotion';
+import { ProcessHttpmsgProvider } from '../providers/process-httpmsg/process-httpmsg';
+
+import { HttpModule } from '@angular/http';
+import { baseURL } from '../shared/baseurl';
+
 
 @NgModule({
   declarations: [
@@ -17,11 +25,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     AboutPage,
     MenuPage,
-    ContactPage
+    ContactPage,
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +38,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     AboutPage,
     MenuPage,
-    ContactPage
+    ContactPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    { provide: 'BaseURL', useValue: baseURL },
+    DishProvider,
+    LeaderProvider,
+    PromotionProvider,
+    ProcessHttpmsgProvider
   ]
 })
 export class AppModule {}
---
+++ src/shared/leader.ts
@@ -0,0 +1,9 @@
+export interface Leader {
+    id: number;
+    name: string;
+    image: string;
+    destignation: string;
+    abbr: string;
+    featured: boolean;
+    description: string;
+}
