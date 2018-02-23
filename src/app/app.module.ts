1ca97f9cc2536a9957ef912bcc4537321149c9cf
Xiaolong Huang <xiaolong_huang@foxmail.com>
2018-02-24 09:56:35 +1300

    Camera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FavoritesPage } from '../pages/favorites/favorites';
 import { ReservationPage } from '../pages/reservation/reservation';
 import { CommentPage } from '../pages/comment/comment';
 import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -29,6 +30,8 @@ import { IonicStorageModule } from '@ionic/storage';
 import { LocalNotifications } from '@ionic-native/local-notifications';
 import { EmailComposer } from '@ionic-native/email-composer';
 import { SocialSharing } from '@ionic-native/social-sharing';
+import { Camera } from '@ionic-native/camera';
+
 
 
 
@@ -46,6 +49,7 @@ import { SocialSharing } from '@ionic-native/social-sharing';
     ReservationPage,
     CommentPage,
     LoginPage,
+    RegisterPage,
   ],
   imports: [
     BrowserModule,
@@ -65,6 +69,7 @@ import { SocialSharing } from '@ionic-native/social-sharing';
     ReservationPage,
     CommentPage,
     LoginPage,
+    RegisterPage,
   ],
   providers: [
     StatusBar,
@@ -78,7 +83,8 @@ import { SocialSharing } from '@ionic-native/social-sharing';
     { provide: 'BaseURL', useValue: baseURL },
     FavoriteProvider,
     EmailComposer,
-    SocialSharing
+    SocialSharing,
+    Camera
   ]
 })
 export class AppModule {}
